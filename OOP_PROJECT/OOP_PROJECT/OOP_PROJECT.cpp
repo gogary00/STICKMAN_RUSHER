8eb950efe0306e0ee193b1e5efc09a591ffd5f94
twm0989256926 <t108590031@ntut.org.tw>
2021-03-11 20:45:35 +0800

    final test

--- OOP_PROJECT/OOP_PROJECT/OOP_PROJECT.cpp
+++ OOP_PROJECT/OOP_PROJECT/OOP_PROJECT.cpp
@@ -1,9 +1,25 @@
 #include "stdafx.h"
 #include <string>
 #include <iostream>
+
 using namespace System;
+using namespace std;
 
 bool Guess(int number) {
+	static int target = -1;
+	if (target==-1) {
+		Random r;
+		target = r.Next() % 100 + 1;
+	}
+
+	if (number==target) {
+		cout << "Correct !!";
+		target = -1;
+		return true;
+	}
+	else {
+		cout << "Wrong" << endl;
+	}
 	return false;
 }
 int main(array<System::String ^> ^args){
