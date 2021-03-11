f7dd4b3caa686f7db5887bab1df0a468799e9aea
twm0989256926 <t108590031@ntut.org.tw>
2021-03-11 21:08:11 +0800

    commit 3

--- OOP_PROJECT/OOP_PROJECT/OOP_PROJECT.cpp
+++ OOP_PROJECT/OOP_PROJECT/OOP_PROJECT.cpp
@@ -3,15 +3,23 @@
 #include <cstdlib>
 #include <ctime>
 
+
 bool Guess(int number) {
+	static int target = -1;
+	srand(time(NULL));
+	if (target == -1) {
+		target = rand() % 100 + 1;
+	}
+
+	if (number == target) {
+		return true;
+	}
 	return false;
 }
-int main()
-{
+int main(array<System::String ^> ^args) {
 	int guess;
 	do {
-		std::cout << "Choose a number between 1 - 100 :";
 		std::cin >> guess;
 	} while (!Guess(guess));
-	return 1;
+	return 0;
 }
