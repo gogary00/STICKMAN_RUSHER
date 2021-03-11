9b0d8b78d4ff110acd048d0fddb3c175e13f75d8
gogary00 <t108590030@ntut.org.tw>
2021-03-11 20:27:08 +0800

    second commit

--- OOP_PROJECT/OOP_PROJECT/OOP_PROJECT.cpp
+++ OOP_PROJECT/OOP_PROJECT/OOP_PROJECT.cpp
@@ -1,15 +1,15 @@
-﻿#include "pch.h"
-//#include "stdafx.h"
-#include <iostream>
+#include "stdafx.h"
 #include <string>
-
+#include <iostream>
 using namespace System;
 
 bool Guess(int number) {
-	
+	return false;
 }
-
-int main(array<System::String ^> ^args)
-{
-    return 0;
+int main(array<System::String ^> ^args){
+	int guess;
+	do {
+		std::cin >> guess;
+	} while (!Guess(guess));
+	return 0;
 }
