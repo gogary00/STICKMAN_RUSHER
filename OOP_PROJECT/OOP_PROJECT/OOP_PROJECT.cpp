8a5d40e2e0dcd9cdff84f551008b15480e047bcc
twm0989256926 <t108590031@ntut.org.tw>
2021-03-11 20:11:54 +0800

    Toturial 2

---
+++ OOP_PROJECT/OOP_PROJECT/OOP_PROJECT.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+//#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace System;
+
+bool Guess(int number) {
+	
+}
+
+int main(array<System::String ^> ^args)
+{
+    return 0;
+}
